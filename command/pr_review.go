19cd28379f246982e9edbf2b49787f282484d84b
vilmibm <vilmibm@neongrid.space>
2020-05-04 16:51:12 -0500

    add cmd

--- command/pr_review.go
+++ command/pr_review.go
@@ -1,4 +1,27 @@
 package command
 
+import (
+	"github.com/spf13/cobra"
+)
+
 func init() {
+	prCmd.AddCommand(prReviewCmd)
+
+	prCmd.Flags().StringP("approve", "a", "", "Approve pull request")
+	prCmd.Flags().StringP("request-changes", "r", "", "Request changes on a pull request")
+	prCmd.Flags().StringP("comment", "c", "", "Comment on a pull request")
+}
+
+var prReviewCmd = &cobra.Command{
+	Use:   "TODO",
+	Short: "TODO",
+	Long:  "TODO",
+	RunE:  prReview,
+}
+
+func prReview(cmd *cobra.Command, args []string) error {
+
+	// TODO process flags, make some decisions
+
+	return nil
 }
